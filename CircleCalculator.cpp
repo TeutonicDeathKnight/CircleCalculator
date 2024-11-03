940bbc94e79a5396aed2d76b083311910ac8020b
Zachary Seeley <zseeley7913@gmail.com>
2024-11-03 11:35:32 -0600

    Add project files.

---
+++ CircleCalculator.cpp
@@ -0,0 +1,12 @@
+// CircleCalculator.cpp -- Calculate and display circle details based on user input
+//CSIS 212-<D02_202440>
+//<Citations if necessary> -- citations are required
+//  for any references you used (outside of your book, 
+//  the website associated with your book, or references
+//  provided in class/Canvas).
+
+//Include statements
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
